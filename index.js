c1fc55d2118647a8084cc5dfc42c9b5e95fca19d
InDaPlaZ <benjaminspider@hotmail.fr>
2022-05-12 13:24:56 +0200

    goku

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const createIndexFile = () => {
     if (!fs.existsSync('build')) {
         fs.mkdirSync('build')
     }
-   
-    
-    fs.writeFileSync('build/index.html', src="https://raw.githubusercontent.com/benjaminscheubel/deploy2/main/img/static-assets-upload15450755136710713434.webp");
+    const str = '<img class="fit-picture"src="https://raw.githubusercontent.com/benjaminscheubel/deploy2/main/img/static-assets-upload15450755136710713434.webp" alt="GOKU SAIYAN ROSE">';
+    fs.writeFileSync('build/index.html', str);
 };
 
 createIndexFile();
