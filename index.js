45c24b0c6df703e8543e89f43667103631ecaad8
InDaPlaZ <benjaminspider@hotmail.fr>
2022-05-12 13:43:11 +0200

    djadja

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const createIndexFile = () => {
     if (!fs.existsSync('build')) {
         fs.mkdirSync('build')
     }
-    const str = '<video controls><source src="../img/2714104d62f14fa0a21aaa12c6d65612.mp4" type="video/mp4"><p>Votre navigateur ne prend pas en charge les vidéos HTML5.Voici <a href="../img/2714104d62f14fa0a21aaa12c6d65612.mov">un lien pour télécharger la vidéo</a>.</p></video>';
+    const str = '<a href="https://cdn.discordapp.com/attachments/905140310571630642/972845964652081163/2714104d62f14fa0a21aaa12c6d65612.mov">link text</a>';
     fs.writeFileSync('build/index.html', str);
 };
 
