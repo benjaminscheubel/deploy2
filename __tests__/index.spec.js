6fb5e4cefcaa1da55ea46349e5681679d8399955
InDaPlaZ <benjaminspider@hotmail.fr>
2022-05-12 11:13:46 +0200

    code ok

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-undef */
 const fs = require('fs');
 const createIndexFile = require('../index');
 
 test("hello_world", () => {
     createIndexFile();
 
+    // eslint-disable-next-line no-undef
     const content = fs.readFileSync(`${__dirname}/../build/index.html`, 'utf-8');
 
     expect(content).toEqual('oeoeoe');
