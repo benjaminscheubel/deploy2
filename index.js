5b3f2fdd4e1e0ff2959c024aca276235a2cd8271
InDaPlaZ <benjaminspider@hotmail.fr>
2022-05-12 14:03:40 +0200

    djadja

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const createIndexFile = () => {
     if (!fs.existsSync('build')) {
         fs.mkdirSync('build')
     }
-    const str = '<video controls><source src="../img/video-1652356439.mp4" type="video/mp4"><source src="../img/video-1652356439.webm" type="video/webm"><p>Votre navigateur ne prend pas en charge les vidéos HTML5.Voici <a href="../img/2714104d62f14fa0a21aaa12c6d65612.mov">un lien pour télécharger la vidéo</a>.</p></video>';
+    const str = '<video controls><source src="../img/video-1652356439.mp4" type="video/mp4"><source src="../img/video-1652356439.ogv" type="video/ogg"><source src="../img/video-1652356439.webm" type="video/webm"><p>Votre navigateur ne prend pas en charge les vidéos HTML5.Voici <a href="../img/2714104d62f14fa0a21aaa12c6d65612.mov">un lien pour télécharger la vidéo</a>.</p></video>';
     fs.writeFileSync('build/index.html', str);
 };
 
